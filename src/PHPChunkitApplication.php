1e284750e99f86d1d1cc1f5ed2646de125e4fcfb
Jonathan H. Wage <jonwage@gmail.com>
2016-10-26 17:47:45 -0400

    Update more descriptions.

--- src/PHPChunkitApplication.php
+++ src/PHPChunkitApplication.php
@@ -31,10 +31,10 @@ class PHPChunkitApplication
         $this->app->register('filter')
             ->setDescription('Run tests that match a filter.')
             ->addArgument('filter', InputArgument::OPTIONAL, 'Filter to run.')
-            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode')
-            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for each chunk', '750M')
-            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error')
-            ->addOption('failed', null, InputOption::VALUE_NONE, 'Track tests that have failed')
+            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode.')
+            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for PHP.', '256M')
+            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error.')
+            ->addOption('failed', null, InputOption::VALUE_NONE, 'Track tests that have failed.')
             ->setCode(function($input, $output) use ($testRunner) {
                 return $testRunner->runFilteredFiles($input->getArgument('filter'));
             })
@@ -43,10 +43,10 @@ class PHPChunkitApplication
         $this->app->register('file')
             ->setDescription('Run a single test file.')
             ->addArgument('file', InputArgument::OPTIONAL, 'File to run.')
-            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode')
-            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for each chunk', '750M')
-            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error')
-            ->addOption('failed', null, InputOption::VALUE_NONE, 'Track tests that have failed')
+            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode.')
+            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for PHP.', '256M')
+            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error.')
+            ->addOption('failed', null, InputOption::VALUE_NONE, 'Track tests that have failed.')
             ->setCode(function($input, $output) use ($testRunner) {
                 return $testRunner->runPhpunit($input->getArgument('file'));
             })
@@ -54,23 +54,23 @@ class PHPChunkitApplication
 
         $this->app->register('watch')
             ->setDescription('Watch for changes to files and run the associated tests.')
-            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode')
-            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for each chunk', '750M')
-            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error')
-            ->addOption('failed', null, InputOption::VALUE_REQUIRED, 'Track tests that have failed', true)
+            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode.')
+            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for PHP.', '256M')
+            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error.')
+            ->addOption('failed', null, InputOption::VALUE_REQUIRED, 'Track tests that have failed.', true)
             ->setCode([new Command\TestWatcher($testRunner, $this->configuration), 'execute'])
         ;
 
         $this->app->register('run')
             ->setDescription('Run tests.')
-            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode')
-            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for each chunk', '1.5G')
-            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error')
-            ->addOption('create-dbs', null, InputOption::VALUE_NONE, 'Create the test databases before running tests')
-            ->addOption('sandbox', null, InputOption::VALUE_NONE, 'Configure unique names')
+            ->addOption('debug', null, InputOption::VALUE_NONE, 'Run tests in debug mode.')
+            ->addOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for PHP.', '256M')
+            ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop on failure or error.')
+            ->addOption('create-dbs', null, InputOption::VALUE_NONE, 'Create the test databases before running tests.')
+            ->addOption('sandbox', null, InputOption::VALUE_NONE, 'Configure unique names.')
             ->addOption('chunk', null, InputOption::VALUE_REQUIRED, 'Run a specific chunk of tests.')
             ->addOption('num-chunks', null, InputOption::VALUE_REQUIRED, 'The number of chunks to run tests in.', 1)
-            ->addOption('failed', null, InputOption::VALUE_NONE, 'Track tests that have failed')
+            ->addOption('failed', null, InputOption::VALUE_NONE, 'Track tests that have failed.')
             ->addOption('group', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Run all tests in these groups.')
             ->addOption('exclude-group', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Run all tests excluding these groups.')
             ->addOption('changed', null, InputOption::VALUE_NONE, 'Run changed tests.')
@@ -79,7 +79,7 @@ class PHPChunkitApplication
 
         $this->app->register('create-dbs')
             ->setDescription('Create the test databases.')
-            ->addOption('sandbox', null, InputOption::VALUE_NONE, 'Configure unique names')
+            ->addOption('sandbox', null, InputOption::VALUE_NONE, 'Prepare sandbox before creating databases.')
             ->setCode([new Command\CreateDatabases($this->configuration->getEventDispatcher()), 'execute'])
         ;
 
