7d11c0438d7121ba3fde6b130dcfe8498bb7a235
Paul McNeely <paul@paulmcneely.com>
2017-12-06 23:36:54 -0600

    Updated composer.json requirements. Changed compatibility in bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+if (class_exists('PHPUnit_Framework_TestCase') && !class_exists('PHPUnit\Framework\TestCase')) {
+    class_alias( 'PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
